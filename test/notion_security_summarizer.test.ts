5182104491112d59950b6cfa040b05be2a436cdd
raiha <68338762+raihalea@users.noreply.github.com>
2025-01-07 05:20:31 +0900

    Initial commit

---
+++ test/notion_security_summarizer.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as NotionSecuritySummarizer from '../lib/notion_security_summarizer-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/notion_security_summarizer-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new NotionSecuritySummarizer.NotionSecuritySummarizerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
